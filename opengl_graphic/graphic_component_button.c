fc887e6bd0dfda0b49e3d660d6ffa261670ca96b
xlnx <963504621@qq.com>
2017-03-20 22:12:46 +0800

    why no commit record?!

--- opengl_graphic/graphic_component_button.c
+++ opengl_graphic/graphic_component_button.c
@@ -25,7 +25,7 @@ def_constructor(ogg_button, args)
 static void button_click(ogg_button* button, va_list args, ogg_handle_flag *handled)
 {
     ogg_coord coord; coord.x = va_arg(args, int); coord.y = va_arg(args, int);
-    if (1) {    // 坐标转换函数 等我想想
+    if (1) {
         button->callback(button);
         *handled = ogg_true;
     }
